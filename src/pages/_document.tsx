8754c33d311edc642de4451784732bb3f1f515b5
avvakumovid <avvakumovid@gmail.com>
2023-04-11 17:40:40 +0300

    feat: add meta and change sections heading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta name='theme-color' content='#010101' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='#010101' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='#010101' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
