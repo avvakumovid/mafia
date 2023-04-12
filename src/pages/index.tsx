406826e2ad3f5ec7f2aff0be8836ad6a8a6766b8
avvakumovid <avvakumovid@gmail.com>
2023-04-12 14:51:07 +0300

    fix: gallery sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import builds from '../../public/builds.png';
 import cars from '../../public/cars.png';
 import bridge from '../../public/bridge.png';
 import cards from '../../public/cards.jpg';
-import table from '../../public/table.jpg';
-import s from '../../public/174.jpg';
 import ff from '../../public/ivan.jpg';
+import chicago from '../../public/chicago.jpg';
+import newYork from '../../public/new-york.jpg';
+import sicilia from '../../public/sicilia.jpg';
+import paris from '../../public/paris.jpg';
+
 import { useEffect } from 'react';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -85,105 +88,141 @@ export default function Home() {
           </p>
         </div>
       </section>
-      <section className='gallery section'>
-        <div className='gallery__content content'>
-          <h2 className='gallery__heading heading'>Галерея</h2>
-          <p className='gallery__paragraph paragraph'>
-            Наши фотографии - это лучший способ узнать о нашем клубе и игре
-            "Мафия". Мы с удовольствием делимся своими снимками, чтобы показать,
-            как мы проводим время, играя в игру, и чтобы познакомиться с нашими
-            участниками. Вы можете увидеть фотографии на нашем сайте и
-            ознакомиться с нашими последними играми.
-          </p>
-        </div>
-        <Swiper
-          pagination={{
-            dynamicBullets: true,
-          }}
-          modules={[Pagination]}
-          className='mySwiper'
-        >
-          <SwiperSlide>
-            <Image className='gallery__image' alt='intro' src={cards} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image className='gallery__image' alt='intro' src={cards} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image className='gallery__image' alt='intro' src={cards} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image className='gallery__image' alt='intro' src={cards} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image className='gallery__image' alt='intro' src={cards} />
-          </SwiperSlide>
-        </Swiper>
-      </section>
       <section className='location section'>
         <h2 className='location__heading heading'>Локации</h2>
         <div></div>
       </section>
-      <div className='container'>
-        <div className='box-1'>
-          <div className='gallery__heading content'>
-            <h2 className='gallery__header heading'>Галерея</h2>
-            <p className='gallery__paragraph paragraph'>
-              Наши фотографии - это лучший способ узнать о нашем клубе и игре
-              "Мафия". Мы с удовольствием делимся своими снимками, чтобы
-              показать, как мы проводим время, играя в игру, и чтобы
-              познакомиться с нашими участниками. Вы можете увидеть фотографии
-              на нашем сайте и ознакомиться с нашими последними играми.
-            </p>
+      <section className='gallery section'>
+        <div className='container'>
+          <div className='box-1'>
+            <div className='gallery__heading content'>
+              <h2 className='gallery__heading heading'>Галерея</h2>
+              <p className='gallery__paragraph '>
+                Наши фотографии - это лучший способ узнать о нашем клубе и игре
+                "Мафия". Мы с удовольствием делимся своими снимками, чтобы
+                показать, как мы проводим время, играя в игру, и чтобы
+                познакомиться с нашими участниками. Вы можете увидеть фотографии
+                на нашем сайте и ознакомиться с нашими последними играми.
+              </p>
+            </div>
+          </div>
+          <div className='box-2'>
+            <Image
+              alt='cars'
+              src={sicilia}
+              placeholder='blur'
+              unoptimized
+              quality={100}
+              fill
+              sizes='100vw'
+              style={{
+                objectFit: 'cover',
+                objectPosition: 'center center',
+              }}
+            />
+          </div>
+          <div className='box-3'>
+            <Image
+              alt='cars'
+              src={paris}
+              placeholder='blur'
+              unoptimized
+              quality={100}
+              fill
+              sizes='100vw'
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+          <div className='box-4'>
+            <Swiper
+              pagination={{
+                dynamicBullets: true,
+              }}
+              modules={[Pagination]}
+              className='mySwiper'
+            >
+              <SwiperSlide>
+                <div className='swiper__slide'>
+                  <Image
+                    alt='cars'
+                    src={chicago}
+                    placeholder='blur'
+                    unoptimized
+                    quality={100}
+                    fill
+                    sizes='100vw'
+                    style={{ objectFit: 'cover' }}
+                  />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='swiper__slide'>
+                  <Image
+                    alt='cars'
+                    src={newYork}
+                    placeholder='blur'
+                    unoptimized
+                    quality={100}
+                    fill
+                    sizes='100vw'
+                    style={{ objectFit: 'cover' }}
+                  />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='swiper__slide'>
+                  <Image
+                    alt='cars'
+                    src={sicilia}
+                    placeholder='blur'
+                    unoptimized
+                    quality={100}
+                    fill
+                    sizes='100vw'
+                    style={{ objectFit: 'cover' }}
+                  />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='swiper__slide'>
+                  <Image
+                    alt='cars'
+                    src={paris}
+                    placeholder='blur'
+                    unoptimized
+                    quality={100}
+                    fill
+                    sizes='100vw'
+                    style={{ objectFit: 'cover' }}
+                  />
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+          <div className='box-5'>
+            <Image
+              alt='cars'
+              src={newYork}
+              placeholder='blur'
+              unoptimized
+              quality={100}
+              fill
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+          <div className='box-6'>
+            <Image
+              alt='cars'
+              src={newYork}
+              placeholder='blur'
+              unoptimized
+              quality={100}
+              fill
+              style={{ objectFit: 'cover' }}
+            />
           </div>
         </div>
-        <div className='box-2'>
-          <Image
-            alt='cars'
-            src={cards}
-            placeholder='blur'
-            unoptimized
-            quality={100}
-            fill
-            sizes='100vw'
-            style={{
-              objectFit: 'cover',
-              objectPosition: 'center center',
-            }}
-          />
-        </div>
-        <div className='box-3'>
-          <Image
-            alt='cars'
-            src={cards}
-            placeholder='blur'
-            unoptimized
-            quality={100}
-            fill
-            sizes='100vw'
-            style={{ objectFit: 'cover' }}
-          />
-        </div>
-        <div className='box-4'>
-          <Image
-            alt='cars'
-            src={ff}
-            placeholder='blur'
-            unoptimized
-            quality={100}
-            fill
-            sizes='100vw'
-            style={{ objectFit: 'cover' }}
-          />
-        </div>
-      </div>
-
-      <div
-        style={{
-          width: 500,
-          height: 3000,
-        }}
-      ></div>
+      </section>
     </>
   );
 }
