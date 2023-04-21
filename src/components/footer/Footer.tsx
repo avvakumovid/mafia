21ffef0a712c661a35e6def24fb04fec6bb999b6
avvakumovid <avvakumovid@gmail.com>
2023-04-22 00:53:21 +0300

    feat: change footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,6 +2,7 @@ import styles from './Footer.module.css';
 import Image from 'next/image';
 import tg from '../../../public/telegram.svg';
 import inst from '../../../public/instagram.svg';
+import wa from '../../../public/whatsapp.svg';
 
 export const Footer = () => {
   return (
@@ -22,6 +23,16 @@ export const Footer = () => {
               target='_blank'
             >
               @mafia_novaya_riga
+            </a>
+          </span>
+          <span>Для записи на игру пшите в Whats App</span>
+          <span className={styles.link}>
+            <Image width={40} height={40} alt='whatsapp' src={wa} />
+            <a
+              href='https://wa.me/79183073562?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82!%20%F0%9F%91%8B%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%9C%D0%B0%D1%84%D0%B8%D1%8F!'
+              target='_blank'
+            >
+              +7 918 307-35-62
             </a>
           </span>
         </div>
