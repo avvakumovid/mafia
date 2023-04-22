9bdc463fe6b9a5c6082f67ed42f3638360241d6b
avvakumovid <avvakumovid@gmail.com>
2023-04-22 12:34:54 +0300

    feat[video-player]: play only one video

--- src/components/ui/video-player/VideoPlayer.tsx
+++ src/components/ui/video-player/VideoPlayer.tsx
@@ -1,4 +1,9 @@
-import { DetailedHTMLProps, VideoHTMLAttributes } from 'react';
+import {
+  DetailedHTMLProps,
+  VideoHTMLAttributes,
+  useEffect,
+  useRef,
+} from 'react';
 import styles from './VideoPlayer.module.css';
 
 interface VideoPlayerProps
@@ -10,8 +15,24 @@ interface VideoPlayerProps
 }
 
 export const VideoPlayer = ({ src, ...rest }: VideoPlayerProps) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   return (
-    <video {...rest} controls>
+    <video
+      {...rest}
+      controls
+      id={src}
+      ref={videoRef}
+      onPlay={e => {
+        console.log('ss');
+        const videos = document.querySelectorAll('video');
+        videos.forEach(v => {
+          if (v !== e.target) {
+            v.pause();
+          }
+        });
+      }}
+    >
       <source src={src} type='video/mp4' />
     </video>
   );
